a3cd02e4e436618ef2f6e3f80a23d21dfcf622bd
krishna723 <77148449+krishna723@users.noreply.github.com>
2023-02-15 20:45:34 +0530

    Add comment on DrawingView class

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -6,6 +6,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -33,7 +34,7 @@ class DrawingView (context: Context, attrs: AttributeSet): View(context,attrs) {
         mDrawPaint!!.strokeJoin= Paint.Join.ROUND
         mDrawPaint!!.strokeCap= Paint.Cap.ROUND
         mCanvasPaint= Paint(Paint.DITHER_FLAG)
-        mBrushSize=20.toFloat()
+        //mBrushSize=20.toFloat()
 
 
 
@@ -89,6 +90,11 @@ class DrawingView (context: Context, attrs: AttributeSet): View(context,attrs) {
         }
         invalidate()
         return true
+    }
+
+    fun setSizeForBrush(newSize:Float){
+        mBrushSize=TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,newSize,resources.displayMetrics)
+        mDrawPaint!!.strokeWidth=mBrushSize
     }
     internal inner class CustomPath(var color: Int, var brushThickness: Float): android.graphics.Path() {
 
--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.kidsdrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private var drawingView:DrawingView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawingView=findViewById(R.id.drawing_View)
+        drawingView?.setSizeForBrush(5.toFloat())
+    }
+
+    private fun showBrushSizeChooserDialog(){
+
+        val brushDialog= Dialog(this)
+        brushDialog.setContentView(R.id.check)
+
+        brushDialog.setTitle("Select brush size: ")
+
+
     }
 }
